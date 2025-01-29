dc9dc99705d1bc3f409424018b2b2e601506836a
niksingh710 <nik.singh710@gmail.com>
2025-01-29 09:45:51 +0530

    Update: Improved the code in way of syntax

--- flake.nix
+++ flake.nix
@@ -12,31 +12,58 @@
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, self, ... }:
         let
-          dfs = input: seen:
-            if builtins.hasAttr "inputs" input then
-              let
-                inputs = builtins.mapAttrs
-                  (name: value:
-                    if builtins.hasAttr name seen then
-                      seen.${name}
-                    else
-                      dfs value (seen // { "${name}" = value; })
-                  )
-                  input.inputs;
-              in
-              {
-                path = input;
-                inputs = inputs;
-              }
-            else
-              {
-                path = input;
-              };
+          # better dfs approach
+          bdfs = input: seen:
+            let
+              isFlake = builtins.hasAttr "inputs" input;
+              inputs =
+                if isFlake then
+                  builtins.mapAttrs
+                    (name: value:
+                      if builtins.hasAttr name seen then
+                        seen.${name}
+                      else
+                        bdfs value (seen // { "${name}" = value; })
+                    )
+                    input.inputs
+                else { };
+            in
+            {
+              path = input;
+              inputs = inputs;
+              flake = isFlake;
+            };
+
+          # Older dfs approach
+          # dfs = input: seen:
+          #   if builtins.hasAttr "inputs" input then
+          #     let
+          #       inputs = builtins.mapAttrs
+          #         (name: value:
+          #           if builtins.hasAttr name seen then
+          #             seen.${name}
+          #           else
+          #             dfs value (seen // {
+          #               "${name}" = value;
+          #             })
+          #         )
+          #         input.inputs;
+          #     in
+          #     {
+          #       path = input;
+          #       inputs = inputs;
+          #       flake = true;
+          #     }
+          #   else
+          #     {
+          #       path = input;
+          #       flake = false;
+          #     };
         in
         {
           packages.default = pkgs.writeText "output.json"
             ''
-              ${builtins.toJSON (dfs inputs.flake {})}
+              ${builtins.toJSON (bdfs inputs.flake {})}
             '';
         };
     };
